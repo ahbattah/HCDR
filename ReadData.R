ad2cba826dc22516e4e0e95e6621612759123c12
unknown <ah.battah@gmail.com>
2018-07-15 22:35:23 +0300

    add home

--- ReadData.R
+++ ReadData.R
@@ -14,6 +14,16 @@ train <- read_csv("F:\\R_projects\\HomeCredit_files\\application_train.csv")
 test <- read_csv("F:\\R_projects\\HomeCredit_files\\application_test.csv")
 
 
+# Home
+bureau_balance <- read_csv("E:\\R\\files\\HomeCredit\\bureau_balance.csv") 
+bureau <- read_csv("E:\\R\\files\\HomeCredit\\bureau.csv")
+cc_balance <- read_csv("E:\\R\\files\\HomeCredit\\credit_card_balance.csv")
+payments <- read_csv("E:\\R\\files\\HomeCredit\\installments_payments.csv") 
+pc_balance <- read_csv("E:\\R\\files\\HomeCredit\\POS_CASH_balance.csv")
+previous <- read_csv("E:\\R\\files\\HomeCredit\\previous_application.csv")
+train <- read_csv("E:\\R\\files\\HomeCredit\\application_train.csv") 
+test <- read_csv("E:\\R\\files\\HomeCredit\\application_test.csv")
+
 # NA count and percentage
 # na_count <- sapply(train, function(x) sum(length(which(is.na(x)))))
 # na_percentage <- sapply(train, function(x) format(100 * sum(length(which(is.na(x)))) 
